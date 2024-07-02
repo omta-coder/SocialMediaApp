89aaacf84ebf8f6764ddc0f6f5db2b11d05febc5
omta <omtadayma2001@gmail.com>
2024-07-02 17:35:22 +0530

    Create post Route

--- routes/post.js
+++ routes/post.js
@@ -3,7 +3,7 @@ const { isLoggedIn } = require("../middleware/auth");
 const router = express.Router()
 
 router.get("/create",isLoggedIn,async(req,res,next)=>{
-res.render("creatPost",{title:"Creat Post || Social Media",user:req.user})
+res.render("createPost",{title:"Creat Post || Social Media",user:req.user})
 })
 
 module.exports = router;
