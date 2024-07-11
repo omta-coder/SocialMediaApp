34fe29e3572c76fe1d090038d781e2160f215016
Omta <omtadayma2001@gmail.com>
2024-07-11 17:15:29 +0530

    Complete One to One Chat

---
+++ models/messageModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const messageSchema = mongoose.Schema({
+
+    sender: {
+        type: String,
+        required: true
+    },
+    receiver: {
+        type: String,
+    },
+
+    text: {
+        type: String,
+        required: true
+    }
+
+})
+module.exports = mongoose.model("Message", messageSchema);
