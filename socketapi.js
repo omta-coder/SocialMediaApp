7dcb4d629a1fc241939c1e4ee61791698614ecdb
Omta <omtadayma2001@gmail.com>
2024-07-11 17:14:05 +0530

    One to One Chat Complete

--- socketapi.js
+++ socketapi.js
@@ -1,27 +1,69 @@
 const io = require( "socket.io" )();
+const messageModel = require("./models/messageModel");
 const UserCollection = require("./models/userModel");
 const socketapi = {
     io: io
 };
 
 // Add your socket.io logic here!
-io.on( "connection", function( socket ) {
-    console.log( "A user connected" );
-    socket.on('join',async username => {
+io.on("connection", function (socket) {
+    console.log("A user connected")
+
+    socket.on('join', async username => {
         await UserCollection.findOneAndUpdate({
             username
-        },{
-            socketId : socket.id
+        }, {
+            socketId: socket.id
         })
     })
-    socket.on('sony',async (messageObject)=>{
-        const reciever = await UserCollection.findOne({
-            username : messageObject.reciever
+
+    socket.on('sony', async messageObject => {
+
+        const receiver = await UserCollection.findOne({
+            username: messageObject.receiver
         })
-        const socketId = reciever.socketId;
-        socket.to(socketId).emit('max',messageObject)
+
+        const socketId = receiver.socketId
+
+        await messageModel.create({
+            sender: messageObject.sender,
+            receiver: messageObject.receiver,
+            text: messageObject.message
+        })
+
+        socket.to(socketId).emit('max', messageObject)
+
     })
+
+    socket.on('openChat', async userObject => {
+        const { sender, receiver } = userObject
+        const messages = await messageModel.find({
+            $or: [
+                {
+                    sender: sender,
+                    receiver: receiver
+                },
+                {
+                    sender: receiver,
+                    receiver: sender
+                }
+            ]
+        })
+
+        socket.emit('openChat', messages)
+
+    })
+
+
+
 });
 // end of socket.io logic
+
+/* 
+socket - single user/browser
+io- server
+emit - send
+on - receive
+ */
 
 module.exports = socketapi;
